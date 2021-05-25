14a29599f9c258e58f3bc75e3f0e329a99947f40
James Yeates <jimibue@gmail.com>
2021-05-25 11:25:57 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function App() {
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
+  // the first param is a function that will be called when 
+  // component mounts... (for know)
+  // the [] as the argument make this only gets when components
+  useEffect(()=>{
+    console.log('useEffect called')
+    getUsers()
+  },[])
+
+
+
+  const getUsers = async () => {
+     console.log('before axios call with 3 second delay')
+     let response = await axios.get('https://reqres.in/api/users')
+     console.log('after axios call')
+     console.log('response:', response)
+     console.log('response.data:',  response.data)
+     console.log('response.data.data:',  response.data.data)
+     setUsers(response.data.data)
+     setLoading(false)
+  }
+
+
+  const deleteUser = (id) => {
+    // Note: axios call here... skipping over that for now
+    // down the road we would want to remove from the database
+    console.log('deleteUser clicked id:', id)
+    let newUsers = users.filter( user=>{
+      return user.id !== id
+    })
+
+    setUsers(newUsers)
+  }
+  const renderUsers = () => {
+    if(loading){
+      return <p>Loading users please wait</p>
+    } 
+    return users.map( user => {
+      return (
+        <div key={user.id}>
+          {user.email}: id: {user.id}
+          <p>
+            <span onClick={()=> deleteUser(user.id)}>delete</span>
+            <span>update</span>
+            </p>
+        </div>
+      )
+    })
+  }
+  
+  console.log('about to render to DOM')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Users</h1>
+      {renderUsers()}
     </div>
   );
 }
